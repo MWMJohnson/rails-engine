8b50b02187e45641f2b3170250ebd558b612c2bf
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-13 15:05:19 -0600

    add #update action.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -11,6 +11,16 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(Item.create(item_params)), status: :created
   end
 
+  def update 
+    if item_params.has_key?(:merchant_id) && !Merchant.exists?(Merchant.find(params[:merchant_id]).id)
+      render json: Merchant.error.full_messages
+    else
+      item = Item.find(params[:id])
+      item.update(item_params)
+      render json: ItemSerializer.new(item)
+    end
+  end
+
   def destroy
     item = Item.find(params[:id])
     item.destroy
