3f6440d183f73b2aa8ecbf32a3050914c69d9733
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-14 01:22:35 -0600

    fix spelling typo to match up controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
         collection do
           get 'find_all'
         end
-        resources :items, only: [:index], controller: 'merchant/items'
+        resources :items, only: [:index], controller: 'merchants/items'
       end
       resources :items, only: [:index, :show, :create, :update, :destroy] do
         resources :merchant, only: [:index], controller: 'items/merchant'
