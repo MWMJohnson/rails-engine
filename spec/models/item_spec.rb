c4d5983dddfdea855771beb788f34d24efd6ec03
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-14 03:35:08 -0600

    add test for Item.find_item_by_name method.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  before(:each) do 
+    @merchant1 = Merchant.create!(name: "Hair Care")
+    @merchant2 = Merchant.create!(name: "Jewelry")
+
+    @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id)
+    @item_2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: @merchant1.id)
+    @item_3 = Item.create!(name: "Brush", description: "This takes out tangles", unit_price: 5, merchant_id: @merchant1.id)
+    @item_4 = Item.create!(name: "Hair tie", description: "This holds up your hair", unit_price: 1, merchant_id: @merchant1.id)
+    @item_7 = Item.create!(name: "Scrunchie", description: "This holds up your hair but is bigger", unit_price: 3, merchant_id: @merchant1.id)
+    @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+
+    @item_5 = Item.create!(name: "Bracelet", description: "Wrist bling", unit_price: 200, merchant_id: @merchant2.id)
+    @item_6 = Item.create!(name: "Necklace", description: "Neck bling", unit_price: 300, merchant_id: @merchant2.id)
+  end
+
   describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
@@ -11,5 +26,17 @@ RSpec.describe Item, type: :model do
     it { should belong_to :merchant }
     it { should have_many :invoice_items}
     it { should have_many(:invoices).through(:invoice_items) }
+  end
+
+  describe "class methods" do
+    describe ".find_item_by_name" do
+      it "returns all items with matching results" do
+        expect(Item.find_item_by_name("ru")).to eq([@item_3, @item_7])
+      end
+
+      it "returns all items in case_insensitive alaphabetical order " do
+        expect(Item.find_item_by_name("br")).to eq([@item_5, @item_3])
+      end
+    end
   end
 end
